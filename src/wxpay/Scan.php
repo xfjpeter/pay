d4fd7081ddf30d9d13bb301f3e80ad57555ff262
petrer <fsyzxz@163.com>
2018-03-26 18:02:34 +0800

    扫码支付

---
+++ src/wxpay/Scan.php
@@ -0,0 +1,80 @@
+<?php
+namespace johnxu\pay\wxpay;
+
+class Scan
+{
+    /**
+     * Scan pay
+     *
+     * @access public
+     *
+     * @param  Wxpay  $wxpay
+     *
+     * @return johnxu\pay\wxpay\Scan
+     */
+    public function pay(Wxpay $wxpay)
+    {
+        $wxpay->payload = array_merge([
+            'trade_type' => $this->getTradeType(),
+            'product_id' => Support::generateNonceStr(),
+        ], $wxpay->payload);
+        $payload = Support::instance()->arrayToXml(
+            Support::instance()->signature($wxpay->payload, $wxpay->config['key'])
+        );
+        $this->returnData = $wxpay->unifiedorder($payload);
+
+        return $this;
+    }
+
+    /**
+     * Scan mode two
+     *
+     * @access public
+     *
+     * @return mixed
+     */
+    public function getScanTwo()
+    {
+        if ($this->returnData['return_code'] == 'SUCCESS' && $this->returnData['return_msg'] == 'OK') {
+            \PHPQRCode\QRcode::png($this->returnData['code_url']);
+            // return $this->returnData['code_url'];
+        } else {
+            throw new WxpayException("Unifiedorder return error: {$this->returnData['return_msg']}");
+        }
+    }
+
+    /**
+     * Get qrcode string
+     *
+     * @access protected
+     *
+     * @param  string $product_id
+     * @return string
+     */
+    protected function generateQrcodeString(string $product_id)
+    {
+        $param = [
+            'appid'      => $this->config['appid'],
+            'mch_id'     => $this->config['mch_id'],
+            'time_stamp' => time(),
+            'nonce_str'  => Support::instance()->generateNonceStr(),
+            'product_id' => $product_id,
+        ];
+
+        return 'weixin://' . Support::instance()->getStringParam(
+            Support::instance()->signature($param)
+        );
+    }
+
+    /**
+     * Get pay method
+     *
+     * @access public
+     *
+     * @return string
+     */
+    protected function getTradeType()
+    {
+        return 'NATIVE';
+    }
+}
